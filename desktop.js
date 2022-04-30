bdc7057234f5203c3490cc75ad85a54b577cf0ff
Owais <87132814+captain0owais@users.noreply.github.com>
2022-04-30 11:52:53 +0530

    Add files via upload

--- desktop.js
+++ desktop.js
@@ -8,7 +8,25 @@ function preload(){
 function setup(){
     canvas = createCanvas(450 , 350);
     canvas.center();
+
+objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+document.getElementById("status").innerHTML = "STATUS : DETECTING OBJECTS";
 }
+
+function modelLoaded(){
+console.log("cocossd INITIALIZED");
+STATUS = true;
+objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error , result){
+if(error){
+    console.log(error);
+}else{
+    console.log(result);
+}
+}
+
 
 function back(){
     window.location = "index.html";
